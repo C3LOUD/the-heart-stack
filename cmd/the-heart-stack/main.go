5560cf99d178b6a8195520ce4c0d3ab16a9d0c22
Tommy <tommy@veden.dental>
2023-12-25 15:28:01 +0800

    build: clean up todo project

--- cmd/the-heart-stack/main.go
+++ cmd/the-heart-stack/main.go
@@ -12,19 +12,11 @@ func main() {
 	db.Conn()
 	defer db.Close()
 
-	// e.Use(middleware.Logger())
+	e.Use(middleware.Logger())
 	e.Use(middleware.Recover())
 
 	indexHandler := handler.IndexHandler{}
 	e.GET("/", indexHandler.HandleIndex)
-
-	todoHandler := handler.TodoHandler{}
-	e.GET("/todos", todoHandler.HandleTodoPage)
-	e.POST("/todos", todoHandler.HandleCreateTodo)
-	e.PATCH("/todos/:id", todoHandler.HandleFinishTodo)
-	e.DELETE("/todos/:id", todoHandler.HandleDeleteTodo)
-	e.DELETE("/todos/:id/confirm", todoHandler.HandleConfirmDeleteTodo)
-	e.GET("/todo-list", todoHandler.HandleListTodos)
 
 	e.Start(":3000")
 }
